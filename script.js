cae12d88a7e70e71eb15578795e56fcb1e84f58a
Riya Shrestha <riya.shrestha.3@westcliff.edu>
2024-09-20 14:38:23 +0545

    Calculator using html,css,js

---
+++ script.js
@@ -0,0 +1,21 @@
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function calculate() {
+    try {
+        const result = eval(document.getElementById('display').value);
+        document.getElementById('display').value = result;
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
+
+function backspace() {
+    const currentValue = document.getElementById('display').value;
+    document.getElementById('display').value = currentValue.slice(0, -1);
+}
